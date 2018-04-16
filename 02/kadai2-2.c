f953b692b5888e02e658cb699c9eb4da9a11712b
childpearl26 <takerupon32@gmail.com>
2018-04-16 16:27:35 +0900

    class end

--- 02/kadai2-2.c
+++ 02/kadai2-2.c
@@ -6,6 +6,7 @@ int main(void){
   int t;
   FILE *fp;
 
+  X1=10.0;
   r=1.0;
   K=100.0;
   t=0;
@@ -15,15 +16,17 @@ int main(void){
     printf("error\n");
   }
 
-  for(r=1.0;r<=3.0;r+=0.1){
+  for(r=1.0;r<=3.0;r+=0.001){
 
     //fprintf(fp,"r=%.1f\n",r);
 
-    X1=10.0;
+    X1=10;
 
-    for(t=0;t<50;t++){
+    for(t=0;t<300;t++){
       X2=X1+r*(1.0-X1/K)*X1;
-      fprintf(fp,"%d,%.1f\n",t,X2);
+      if(t>=250){
+        fprintf(fp,"%.3f,%.1f\n",r,X2);
+      }
       X1=X2;
     }
 
