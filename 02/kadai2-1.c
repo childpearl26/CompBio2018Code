0baf5e30dcfb47bb4889b99a0a6b3dec283c23db
childpearl26 <takerupon32@gmail.com>
2018-04-16 14:42:02 +0900

    deletion

--- 02/kadai2-1.c
+++ 02/kadai2-1.c
@@ -1,27 +1,33 @@
 #include<stdio.h>
 
-void main(){
+int main(void){
 
-  int X1,X2,r,K,t;
+  double X1,X2,r,K;
+  int t;
   FILE *fp;
 
-  X1=5;
-  r=2;
-  K=200;
+  X1=10.0;
+  r=2.0;
+  K=100.0;
   t=0;
 
-  fp=fopen("output.dat","a");
+  fp=fopen("output.dat","w");
   if(fp==NULL){
     printf("error\n");
   }
 
-  fprintf(fp,"%d,%d\n",t,X2);
+  fprintf(fp,"t,X2\n");
 
   for(t=0;t<300;t++){
-    X2=X1+r*(1-x/k)*X1;
-    fprintf(fp,"%d,%d\n",t,X2);
+    X2=X1+r*(1.0-X1/K)*X1;
+    fprintf(fp,"%d,%.1f\n",t,X2);
+    X1=X2;
   }
 
   fclose(fp);
+
+  printf("COMPLETE\n");
+
+  return 0;
 
 }
